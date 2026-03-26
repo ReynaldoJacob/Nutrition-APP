2f5d6171bfa31a3f079a4af7f8ddb420da30a740
Reynaldo Jacobo <reynaldo.jacobo@cimagroup.com.mx>
2026-03-26 14:19:23 -0600

    fix: force HTTPS baseURL in Axios to prevent Mixed Content error

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,11 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Forzar HTTPS en producción para evitar Mixed Content
+if (window.location.protocol === 'https:') {
+    window.axios.defaults.baseURL = window.location.origin;
+}
+
 // Echo/Pusher deshabilitado temporalmente
 // import Echo from 'laravel-echo';
 // import Pusher from 'pusher-js';
